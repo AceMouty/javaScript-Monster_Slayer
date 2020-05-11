5223311fd0edf4abf282402f9cd1a00ab8b45844
kyle Guerrero <kyleguerrero85@gmail.com>
2020-05-11 16:11:18 -0500

    feat: inflict player damage
    after a player attacks the monster, the monster will inflict back damage
    by default. player now has the ability to either win OR lose

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,37 @@
 
 const MAX_HEALTH = 100;
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
+let inputHealth;
 let monsterHealth = MAX_HEALTH;
 let playerHealth = MAX_HEALTH;
+const STARTING_HEALTH = inputHealth || MAX_HEALTH;
 
-adjustHealthBars(MAX_HEALTH);
+adjustHealthBars(STARTING_HEALTH);
 
 function attackHandler() {
 
+  // attack the monster
   const damage = dealMonsterDamage(ATTACK_VALUE);
   monsterHealth -= damage;
+
+  // Attack the player
+  const  playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  playerHealth -= playerDamage;
+
+
+  if (monsterHealth <= 0 && playerHealth > 0) {
+    alert("You win!");
+  } else if (playerHealth <= 0 && monsterHealth > 0) {
+    alert("You DIED!");
+  } else if (playerHealth <= 0 && monsterHealth <= 0) {
+    alert("ITS A DRAW!");
+  }
+
+
+
+  return
 
 }
 
